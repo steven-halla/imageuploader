7f05fa7ce1e3ade796974b3c02c2777400ddf285
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-04 16:49:21 -0800

    "fixed spellign error, server is now set up"

--- server.js
+++ server.js
@@ -12,6 +12,15 @@ app.post('/upload', (req, res) => {
 
   const file = req.files.file;
 
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if(err) {
+      console.log(err);
+      return res.status(500).send(err);
+    }
+
+    res.json({fileName: file.name, filePath:`/uploads/${file.name}`});
+  });
+
 });
 
 app.listen(5000, () => console.log('Server started'));
