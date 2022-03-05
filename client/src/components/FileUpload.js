3170d8287cf22e5b4a44147b16435dc8d462f347
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-04 18:37:31 -0800

    "base form set up  "

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,15 +1,29 @@
-import React , { Fragment } from 'react'
+import React , { Fragment, useState } from 'react';
+import axios from 'axios';
 
 export const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [filename, setFilename] = useState('Choose File');
 
+  const onChange = event => {
+    setFile(event.target.files[0]);
+    setFilename(event.target.files[0].name);
+  };
 
+  const onSubmit = async event => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+  }
 
   return (
-   <Fragment>
+   <Fragment onSubmit={onSubmit}>
      <form action="">
        <div className="custom-file mb-4">
-         <input type="file" className="custom-file-input" id="customFile"/>
-           <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+         <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
+           <label className="custom-file-label" htmlFor="customFile">
+             {filename}
+           </label>
        </div>
        <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
      </form>
