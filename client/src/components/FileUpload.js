4fa8b80dc06fb6fb30aeb2755b90436a9e33efe4
steven-halla <69405488+steven-halla@users.noreply.github.com>
2022-03-04 18:11:56 -0800

    "setting up form "

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -4,10 +4,11 @@ export const FileUpload = () => {
   return (
    <Fragment>
      <form action="">
-       <div className="custom-file">
+       <div className="custom-file mb-4">
          <input type="file" className="custom-file-input" id="customFile"/>
            <label className="custom-file-label" htmlFor="customFile">Choose file</label>
        </div>
+       <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
      </form>
    </Fragment>
   )
